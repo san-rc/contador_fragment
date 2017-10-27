930a764aed7a2626ff16d2489b50711ab19b9377
Santiago Ramayo <14470231@itcampeche.edu.mx>
2017-10-27 12:41:53 -0500

    contadorApp:se agrego los fragmentos e interfaz grafica. No se añadio manejo de eventos

---
+++ app/src/main/java/com/santiagoramayo/android/contadorfrag/contFragment.java
@@ -0,0 +1,30 @@
+package com.santiagoramayo.android.contadorfrag;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by SAN RC on 27/10/2017.
+ */
+
+public class contFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_cont,container,false);
+
+
+        return v;
+    }
+}
